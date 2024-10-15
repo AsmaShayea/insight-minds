95aa37e7daf3d133eb8d7fcdc579b99f7b828d67
Asma Shayea <asma@Asmas-MacBook-Pro.local>
2024-10-15 20:01:31 +0300

    mongo

--- app/database_manager.py
+++ app/database_manager.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 def get_mongo_connection(db_name='insight_minds'):
-    client = MongoClient('mongodb://localhost:27017/')
+    client = MongoClient('mongodb+srv://asma:Sami8407@insightsmindscluster0.34evged.mongodb.net/?retryWrites=true&w=majority&appName=InsightsMindsCluster0')
     return client[db_name]
